d9224b5f4fd43de9e2dd66792fb12250f5f38d9b
Will Jones <willjones2010@gmail.com>
2019-11-12 15:53:27 -0500

    Gateway actions and validators

--- lib/api/channelGateway/index.ts
+++ lib/api/channelGateway/index.ts
@@ -4,6 +4,8 @@ import { gateway } from '../../validators'
 import { ACTIONS, COMMANDS } from '../../enums'
 
 export class ChannelGateway extends ApiClass {
+
+
   /***********************************
    * Gateway
    ***********************************/
@@ -18,4 +20,42 @@ export class ChannelGateway extends ApiClass {
   get(data, req?, validated?) {
     return this.request(req, data, validated)
   }
+
+  /**
+ * List gateways
+ * @param data
+ * @param req
+ * @param validated
+ */
+  @Action({ method: 'GET', route: 'channels/gateways', validator: gateway[ACTIONS.LIST_GATEWAYS] })
+  list(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+ * Create a Gateway
+ * @param data
+ * @param req
+ * @param validated
+ */
+  @Command({ method: 'POST', route: 'channels/:channel_uuid/gateways', validator: gateway[COMMANDS.CREATE_GATEWAY] })
+  create(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+ * Update a Gateway
+ * @param data
+ * @param req
+ * @param validated
+ */
+  @Command({ method: 'PUT', route: 'channels/:channel_uuid/gateways/:gateway_uuid', validator: gateway[COMMANDS.UPDATE_GATEWAY] })
+  update(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+  /**
+ * SET_CHANNEL_GATEWAY_PRIMARY
+ * 
+*/
+
 }
--- lib/validators/gateway/index.ts
+++ lib/validators/gateway/index.ts
@@ -9,10 +9,22 @@ export const gateway = {
    ***********************************/
 
   // Commands
-  // [COMMANDS.CREATE_GATEWAY]: (data) => Utils.joiPromise(data, gatewaySchema.commands[COMMANDS.CREATE_GATEWAY]),
-  // [COMMANDS.UPDATE_GATEWAY]: (data) => Utils.joiPromise(data, gatewaySchema.commands[COMMANDS.UPDATE_GATEWAY]),
+  [COMMANDS.CREATE_GATEWAY]: {
+    params: (data) => Utils.joiPromise(data, gatewaySchema.commands[COMMANDS.CREATE_GATEWAY].params),
+    body: (data) => Utils.joiPromise(data, gatewaySchema.commands[COMMANDS.CREATE_GATEWAY].body),
+  },
+  [COMMANDS.UPDATE_GATEWAY]: {
+    params: (data) => Utils.joiPromise(data, gatewaySchema.commands[COMMANDS.UPDATE_GATEWAY].params),
+    body: (data) => Utils.joiPromise(data, gatewaySchema.commands[COMMANDS.UPDATE_GATEWAY].body),
+  },
 
   // Actions
-  [ACTIONS.GET_GATEWAY]: (data) => Utils.joiPromise(data, gatewaySchema.actions[ACTIONS.GET_GATEWAY]),
-  // [ACTIONS.LIST_GATEWAYS]: (data) => Utils.joiPromise(data, gatewaySchema.actions[ACTIONS.LIST_GATEWAYS]),
+  [ACTIONS.GET_GATEWAY]: {
+    params: (data) => Utils.joiPromise(data, gatewaySchema.commands[ACTIONS.GET_GATEWAY].params),
+    body: (data) => Utils.joiPromise(data, gatewaySchema.commands[ACTIONS.GET_GATEWAY].body),
+  },
+  [ACTIONS.LIST_GATEWAYS]: {
+    params: (data) => Utils.joiPromise(data, gatewaySchema.commands[ACTIONS.LIST_GATEWAYS].params),
+    body: (data) => Utils.joiPromise(data, gatewaySchema.commands[ACTIONS.LIST_GATEWAYS].body),
+  },
 }
