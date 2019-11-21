6fe836a56a44f192bce5bc1c387ad35d41726234
Will Jones <willjones2010@gmail.com>
2019-11-21 17:51:10 -0500

    typo in actions

--- lib/api/channelPermission/index.ts
+++ lib/api/channelPermission/index.ts
@@ -1,14 +1,15 @@
 import { ApiClass } from '../../ApiClass'
 import { Action, Command } from '../../metadata'
 import { permission } from '../../validators/permission'
+import { ACTIONS } from 'enums'
 export class ChannelPermission extends ApiClass {
 
-    @Command({ method: 'GET', route: '/channels/:channel_uuid/users/:user_uuid/permissions', validator: permission[COMMANDS.LIST_USER_PERMISSIONS] })
+    @Command({ method: 'GET', route: '/channels/:channel_uuid/users/:user_uuid/permissions', validator: permission[ACTIONS.LIST_USER_PERMISSIONS] })
     listUserPermissions(data, req?, validated?) {
         return this.request(req, data, validated)
     }
 
-    @Command({ method: 'GET', route: '/channels/:channel_uuid/roles/:role_name/permissions', validator: permission[COMMANDS.LIST_ROLE_PERMISSIONS] })
+    @Command({ method: 'GET', route: '/channels/:channel_uuid/roles/:role_name/permissions', validator: permission[ACTIONS.LIST_ROLE_PERMISSIONS] })
     listRolePermissions(data, req?, validated?) {
         return this.request(req, data, validated)
     }
