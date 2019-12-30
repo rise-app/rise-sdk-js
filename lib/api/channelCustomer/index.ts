6876f15259641be857ec1399573b47e6ed74d032
Scott Wyatt <scottwyatt86@gmail.com>
2019-12-30 11:48:27 -0600

    feat(customer): adds customer users, events, feeds, sub customers

--- lib/api/channelCustomer/index.ts
+++ lib/api/channelCustomer/index.ts
@@ -1,8 +1,7 @@
 import { ApiClass } from '../../ApiClass'
 import { Command, Action, Paginate, Upload } from '../../metadata'
-import { collection, customer, gateway } from '../../validators'
+import { collection, customer } from '../../validators'
 import { ACTIONS, COMMANDS, EVENTS } from '../../enums'
-import { ChannelAuth } from '../channelAuth'
 
 export class ChannelCustomer extends ApiClass {
   /**********************************
@@ -109,6 +108,87 @@ export class ChannelCustomer extends ApiClass {
   })
   @Paginate()
   searchChannelDescendants(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+
+
+  /**
+   * Get a Given Channel's Customer's Parent Customer
+   * @param data
+   * @param req
+   * @param validated
+   */
+  @Action({
+    method: 'GET',
+    route: 'channels/:channel_uuid/customers/:customer_uuid/parent',
+    validator: customer[ACTIONS.GET_CUSTOMER]
+  })
+  getParent(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * List a Given Channel's Customer's Ancestor Customers
+   * @param data
+   * @param req
+   * @param validated
+   */
+  @Action({
+    method: 'GET',
+    route: 'channels/:channel_uuid/customers/:customer_uuid/ancestors',
+    validator: customer[ACTIONS.LIST_CUSTOMERS]
+  })
+  @Paginate()
+  listAncestors(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * List a Given Channel's Customer's Children Customers
+   * @param data
+   * @param req
+   * @param validated
+   */
+  @Action({
+    method: 'GET',
+    route: 'channels/:channel_uuid/customers/:customer_uuid/children',
+    validator: customer[ACTIONS.LIST_CUSTOMERS]
+  })
+  @Paginate()
+  listChildren(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * List a Given Channel's Customer's Descendant Customers
+   * @param data
+   * @param req
+   * @param validated
+   */
+  @Action({
+    method: 'GET',
+    route: 'channels/:channel_uuid/customers/:customer_uuid/descendants',
+    validator: customer[ACTIONS.LIST_CUSTOMERS]
+  })
+  @Paginate()
+  listDescendants(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * List a Given Channel's Customer's Siblings Customers
+   * @param data
+   * @param req
+   * @param validated
+   */
+  @Action({
+    method: 'GET',
+    route: 'channels/:channel_uuid/customers/:customer_uuid/siblings',
+    validator: customer[ACTIONS.LIST_CUSTOMERS]
+  })
+  @Paginate()
+  listSiblings(data, req?, validated?) {
     return this.request(req, data, validated)
   }
 
@@ -431,16 +511,327 @@ export class ChannelCustomerEvent extends ApiClass {
 }
 
 export class ChannelCustomerFeed extends ApiClass {
+  /**
+   * List Customer's feeds
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/feeds',
+    // validator: customer[ACTIONS.LIST_CUSTOMER_FEEDS]
+  })
+  @Paginate()
+  list(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * List Customer's feeds
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/feeds/:feed_uuid',
+    // validator: customer[ACTIONS.GET_CUSTOMER_FEED]
+  })
+  @Paginate()
+  get(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
 }
 
 export class ChannelCustomerOrder extends ApiClass {
+  /**
+   * List Customer's orders
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/orders',
+    validator: customer[ACTIONS.LIST_CUSTOMER_ORDERS]
+  })
+  @Paginate()
+  list(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * List Customer's orders
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/orders/:order_uuid',
+    // validator: customer[ACTIONS.GET_CUSTOMER_ORDER]
+  })
+  @Paginate()
+  get(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
 }
 
 export class ChannelCustomerGatewayAccount extends ApiClass {
+  /**
+   * List Customer's accounts
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/accounts',
+    validator: customer[ACTIONS.LIST_CUSTOMER_GATEWAY_ACCOUNTS]
+  })
+  @Paginate()
+  list(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * List Customer's accounts
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/accounts/:account_uuid',
+    validator: customer[ACTIONS.GET_CUSTOMER_GATEWAY_ACCOUNT]
+  })
+  @Paginate()
+  get(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * Create Customer's account
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Command({
+    method: 'POST',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/accounts',
+    // validator: customer[COMMANDS.CREATE_CUSTOMER_ACCOUNT]
+  })
+  create(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * Create Customer's account
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Command({
+    method: 'DELETE',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/accounts/:account_uuid',
+    // validator: customer[COMMANDS.REMOVE_CUSTOMER_ACCOUNT]
+  })
+  remove(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
 }
 
 export class ChannelCustomerGatewayAccountSource extends ApiClass {
+
+  /**
+   * List Customer's sources
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/sources',
+    validator: customer[ACTIONS.LIST_CUSTOMER_GATEWAY_ACCOUNT_SOURCES]
+  })
+  @Paginate()
+  list(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * List Customer's sources
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/accounts/:account_uuid/sources',
+    validator: customer[ACTIONS.LIST_CUSTOMER_GATEWAY_ACCOUNT_SOURCES]
+  })
+  @Paginate()
+  listByAccount(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * List Customer's sources
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/sources/:source_uuid',
+    validator: customer[ACTIONS.GET_CUSTOMER_GATEWAY_ACCOUNT_SOURCE]
+  })
+  @Paginate()
+  get(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+  /**
+   * List Customer's sources
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/accounts/:account_uuid/sources/:source_uuid',
+    validator: customer[ACTIONS.GET_CUSTOMER_GATEWAY_ACCOUNT_SOURCE]
+  })
+  @Paginate()
+  getByAccount(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * Create Customer's source
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Command({
+    method: 'POST',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/accounts/:account_uuid/sources',
+    // validator: customer[COMMANDS.CREATE_CUSTOMER_SOURCE]
+  })
+  create(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * Create Customer's source
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Command({
+    method: 'DELETE',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/accounts/:account_uuid/sources/:source_uuid',
+    // validator: customer[COMMANDS.REMOVE_CUSTOMER_SOURCE]
+  })
+  remove(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
 }
 
 export class ChannelCustomerUser extends ApiClass {
+  /**
+   * List Customer's users
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/users',
+    validator: customer[ACTIONS.LIST_CUSTOMER_USERS]
+  })
+  @Paginate()
+  list(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * List Customer's users
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Action({
+    method: 'GET',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/users/:user_uuid',
+    // validator: customer[ACTIONS.GET_CUSTOMER_USER]
+  })
+  @Paginate()
+  get(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * Create Customer's user
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Command({
+    method: 'POST',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/users',
+    // validator: customer[COMMANDS.CREATE_CUSTOMER_USER]
+  })
+  create(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * Create Customer's user
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Command({
+    method: 'PUT',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/users',
+    // validator: customer[COMMANDS.ADD_CUSTOMER_USER]
+  })
+  add(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
+
+  /**
+   * Create Customer's user
+   * @param data
+   * @param req
+   * @param validated
+   */
+  // TODO VALIDATOR
+  @Command({
+    method: 'DELETE',
+    route: '/channels/:channel_uuid/customers/:customer_uuid/users',
+    // validator: customer[COMMANDS.REMOVE_CUSTOMER_USER]
+  })
+  remove(data, req?, validated?) {
+    return this.request(req, data, validated)
+  }
 }
---
+++ test/integration/endpoints/channelCustomer/customerEvent.test.js
@@ -0,0 +1,29 @@
+const { RiSE, EVENTS, ACTIONS } = require('../../../../dist')
+const { url, adminPassword, adminIdentifier, channel_uuid, private_key, public_key } = require('../../../fixtures/constants')
+
+const assert = require('assert')
+
+describe('# RiSE Channel Customer Event API', () => {
+
+  let rise, adminToken, adminSession, userToken, userSession, user, customer
+
+  before((done) => {
+    rise = new RiSE({
+      sandbox: true,
+      public_key: public_key,
+      private_key: private_key
+    })
+
+    rise.authenticateApiUser(
+      channel_uuid,
+      adminIdentifier,
+      adminPassword
+    )
+      .then(res => {
+        adminToken = res.token
+        adminSession = res.session
+        done()
+      })
+      .catch(err => done(err))
+  })
+})
